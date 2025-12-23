e0a5c16d292494fde4974dca39c9906cf9b82054
sanjay-singh <sanjaysingh95945@gmail.com>
2025-12-23 14:31:01 +0530

    solution file added

--- day-02/practice/api_data_fetcher.py
+++ day-02/practice/api_data_fetcher.py
@@ -1,26 +1,29 @@
 import requests
 import json
 
-url = "https://official-joke-api.appspot.com/random_joke"
+def api_data_fetcher():
+    url = "https://official-joke-api.appspot.com/random_joke"
 
-response = requests.get(url)
+    response = requests.get(url)
 
-if response.status_code == 200:
-    data = response.json()
+    if response.status_code == 200:
+        data = response.json()
 
-    # Extract meaningful information
-    joke_data = {
-        "setup": data["setup"],
-        "punchline": data["punchline"]
-    }
+        # Extract meaningful information
+        joke_data = {
+            "setup": data["setup"],
+            "punchline": data["punchline"]
+        }
 
-    # Save to JSON file
-    with open("output.json", "a") as file:
-        json.dump(joke_data, file, indent=4)
+        # Save to JSON file
+        with open("output.json", "a") as file:
+            json.dump(joke_data, file, indent=4)
 
-    print("Joke saved successfully to output.json")
-    print("Setup:", joke_data["setup"])
-    print("Punchline:", joke_data["punchline"])
+        print("Joke saved successfully to output.json")
+        print("Setup:", joke_data["setup"])
+        print("Punchline:", joke_data["punchline"])
 
-else:
-    print("Failed to fetch joke. Status code:", response.status_code)
+    else:
+        print("Failed to fetch joke. Status code:", response.status_code)
+
+api_data_fetcher()
